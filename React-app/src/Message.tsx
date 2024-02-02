4ea607b0bf7dae3d9c847ff91f0fbb2f978fa788
Ankit Nainwal <ankit.nainwal.3150@gmail.com>
2024-02-02 23:57:08 +0530

    first commit

---
+++ React-app/src/Message.tsx
@@ -0,0 +1,10 @@
+//creating react components
+
+//PascalCasing (capitalize the first letter)
+function Message(){
+    //JSX: javaScript XML
+    const name = 'Ankit' ;
+    return <h1>Hello {name}</h1>;
+}
+
+export default Message;
